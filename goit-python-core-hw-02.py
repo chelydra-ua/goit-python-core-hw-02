e384e08bc7e84cede49fa0e11bb17fc3b014a81e
chelydra-ua <lipara.shep@gmail.com>
2022-07-13 22:58:12 +0300

    added working script

--- goit-python-core-hw-02.py
+++ goit-python-core-hw-02.py
@@ -5,22 +5,29 @@ wait_for_number = True
 
 while True:
     elem = input('Enter operand or operator ("=" for output): ')
-
     if elem == '=':
         print(result)
         break
     elif elem == '+' or elem == '-' or elem == '*' or elem == '/':
-        operator = elem
+        if wait_for_number == False:
+            operator = elem
+            wait_for_number = True
+        else:
+            print(f"elem {elem} is not a + or - or * or /")
     else:
         try:
             operand = int(elem)
-            if operator == '+':
-                result = result + operand
-            elif operator == '-':
-                result = result - operand
-            elif operator == '*':
-                result = result * operand
-            elif operator == '/':
-                result = result / operand
+            if wait_for_number == True:
+                if operator == '+':
+                    result = result + operand
+                elif operator == '-':
+                    result = result - operand
+                elif operator == '*':
+                    result = result * operand
+                elif operator == '/':
+                    result = result / operand
+                wait_for_number = False
+            else:
+                print(f"elem {elem} is not a number")
         except ValueError:
             print(f"elem {operand} is not a number")
